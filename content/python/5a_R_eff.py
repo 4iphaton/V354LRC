03a9efb5b3a6ddd4fe2816f51655ceb26c522cac
Jan Ellbracht <jan.ellbracht@tu-dortmund.de>
2017-12-03 19:18:02 +0100

    Initial commit

---
+++ content/python/5a_R_eff.py
@@ -0,0 +1,22 @@
+import numpy as np
+import scipy.constants as const
+from uncertainties import ufloat
+from uncertainties import unumpy as unp
+import math
+
+A, t = np.genfromtxt('../../content/values/',unpack=True)
+
+print(A, t)
+
+t *= 10^(-6)
+
+def f(x, b):
+    return e^(-x) + b
+   # return x*b**2-b**3*y
+
+parameters, pcov = curve_fit(f, A, t)
+print(parameters, np.sqrt(np.diag(pcov)), sep='\n')
+t= np.linspace(0,80000,1000)
+
+plt.plot(t, f(t, *parameters), 'g-', label='Fit')
+plt.savefig('build/plot1.pdf')
