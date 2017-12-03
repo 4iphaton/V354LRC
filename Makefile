d182bfced12ef9ceb8cc267e73c31ec8cfac31de
Max Krebs <maxkrebs1@gmail.com>
2017-12-03 20:24:33 +0100

    plot fettich

--- Makefile
+++ Makefile
@@ -11,8 +11,8 @@ endif
 all: build/main.pdf
 
 # hier Python-Skripte:
-build/plot.pdf: contet/python/5a_R_eff.py matplotlibrc header-matplotlib.tex | build
-	TEXINPUTS="$(call translate,$(pwd):)" python contet/python/5a_R_eff.py
+build/plot1.pdf: content/python/5a_R_eff.py matplotlibrc header-matplotlib.tex | build
+	TEXINPUTS="$(call translate,$(pwd):)" python content/python/5a_R_eff.py
 
 # hier weitere Abhängigkeiten für build/main.pdf deklarieren:
 build/main.pdf: build/plot1.pdf
--- content/python/5a_R_eff.py
+++ content/python/5a_R_eff.py
@@ -2,21 +2,28 @@ import numpy as np
 import scipy.constants as const
 from uncertainties import ufloat
 from uncertainties import unumpy as unp
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import scipy.constants as const
 import math
 
-A, t = np.genfromtxt('../../content/values/',unpack=True)
+t, A = np.genfromtxt('content/values/Amplituden_max_val.txt',unpack=True)
+
 
 print(A, t)
 
-t *= 1e-6
+t/=1000000
 
-def f(x, b):
-    return np.exp(-x) + b
+def f(x, a,b):
+    return np.exp(x*a+b)
    # return x*b**2-b**3*y
 
-parameters, pcov = curve_fit(f, A, t)
-print(parameters, np.sqrt(np.diag(pcov)), sep='\n')
-t= np.linspace(0,80000,1000)
+parameters, pcov = curve_fit(f, t, A)
+errors = np.sqrt(np.diag(pcov))
+print(parameters, errors, sep='\n')
+ln = np.linspace(t[0],t[len(t)-1],5000)
 
-plt.plot(t, f(t, *parameters), 'g-', label='Fit')
+plt.plot(ln, f(ln, *parameters), 'g-', label='Fit')
+plt.plot(t,A,'gx',label='Values')
 plt.savefig('build/plot1.pdf')
+print('----------------')
