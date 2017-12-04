00e6f5ec78a3fd2edafc3f3a43cea5e4ee9e2fcd
Jan Ellbracht <jan.ellbracht@tu-dortmund.de>
2017-12-04 10:17:52 +0100

    5b_rechnung, 5c_plot

--- content/python/5b_rechnung.py
+++ content/python/5b_rechnung.py
@@ -7,13 +7,11 @@ import matplotlib.pyplot as plt
 import scipy.constants as const
 import math
 
-a= ufloat(-2.64611770 , 0.148322358 )*(10**(4))
 L = ufloat(10.11 , 0.03)*(10 **(-3))
 C = ufloat(2.098, 0.006) *(10 **(-9))
 R_1 = ufloat(48.1, 0.1)
 R_2 = ufloat(509.5, 0.5)
 
-T_ex= -1/a
-R_eff = - 2*L*a
+R_ap = unp.sqrt((4*L)/C)#**(0.5)
 
-print ("a", a, "T_ex", T_ex, "R_eff", R_eff)
+print("R_ap", R_ap)
--- content/python/5c_rechnung.py
+++ content/python/5c_rechnung.py
@@ -7,13 +7,19 @@ import matplotlib.pyplot as plt
 import scipy.constants as const
 import math
 
-a= ufloat(-2.64611770 , 0.148322358 )*(10**(4))
 L = ufloat(10.11 , 0.03)*(10 **(-3))
 C = ufloat(2.098, 0.006) *(10 **(-9))
 R_1 = ufloat(48.1, 0.1)
 R_2 = ufloat(509.5, 0.5)
+R_g = 50
 
-T_ex= -1/a
-R_eff = - 2*L*a
+U_0 = 15
+U_m = 39
 
-print ("a", a, "T_ex", T_ex, "R_eff", R_eff)
+#U_c
+q_t = (1/(R_1+50))*unp.sqrt(L/C)/10
+print( q_t )#"U_0", U_0 , "U_c", U_c)
+q_e= U_m/U_0
+
+U_min = U_m/q_t
+print( q_e , U_min)
