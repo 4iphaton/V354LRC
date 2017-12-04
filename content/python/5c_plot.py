a3bb03524605a92fd921398562bca21d4eefdb12
Jan Ellbracht <jan.ellbracht@tu-dortmund.de>
2017-12-04 17:46:44 +0100

    dein Problem

--- content/python/5c_plot.py
+++ content/python/5c_plot.py
@@ -16,6 +16,7 @@ R_g = 50
 U_c , nu = np.genfromtxt('content/values/kondensatorspannung_val.txt',unpack=True)
 
 nu *= 1000
+U_c = U_c/10
 
 print( "U_c", U_c, "nu", nu)
 U_c_log = np.log(U_c)
@@ -23,37 +24,24 @@ U_c_log = np.log(U_c)
 
 U_max = 39
 q_e = U_max/10
-q_t = (1/(R_2+R_g))*unp.sqrt(L/C)
+q_t = (1/(R_2+R_g))*(unp.sqrt(L/C))
 #w_0 = 34000
 #w_res = unp.sqrt((1/(L*C))-((R_2+R_g)**2/(2*(L)**2)))
-
-
-print("t", q_t, "e", q_e, )#"res", w_res )
-#abs. 1
-U_b = np.log((1/np.sqrt(2))*U_max)
-
-#b_e= w_0/q_e
 b_t = (R_2+R_g)/L
-#a_b= (w_res-w_0)/w_res
-#print("breite", b_e, "theo", b_t, "ab", a_b)
-#parameters, pcov = curve_fit(f, U_c, nu)
-#errors = np.sqrt(np.diag(pcov))
-#print(parameters, errors, sep='\n')
-#ln = np.linspace(nu[0],nu[len(nu)-1], 4)
 
-#plt.plot(ln, f(ln, *parameters), 'g-', label='Fit')
-plt.plot(nu , U_c ,'rx',label='Messwerte')
+print("t", q_t, "e", q_e, b_t )#"res", w_res )
+#abs. 1
+U_b = ((1/np.sqrt(2))*(U_max)/10
+
+plt.plot(nu ,U_c ,'rx' ,label='Messwerte' )
 
 
 plt.axhline(U_b)
 #plt.axvline
-#plt.plot(nup , 'rb', label='nup')
-#plt.xscale('log')
-#plt.yscale('log')
 
 plt.xlabel(r'$nu/[1/s]$')
 plt.ylabel(r'$U_c/U$')
 plt.legend(loc='best')
 plt.tight_layout()
 plt.savefig('build/plot2.pdf')
-print('----------------')
+print('-----------------')
