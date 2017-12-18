57d2b032d24605cbd0af4bd6c892fb9afa878c45
Max Krebs <maxkrebs1@gmail.com>
2017-12-18 20:31:48 +0100

    theoplot

--- content/python/5c_plot.py
+++ content/python/5c_plot.py
@@ -13,6 +13,7 @@ R_1 = ufloat(48.1, 0.1)
 R_2 = ufloat(509.5, 0.5)
 R_g = 50
 
+RC = C * R_1
 U_c , nu = np.genfromtxt('content/values/kondensatorspannung_val.txt',unpack=True)
 
 nu *= 1000
@@ -31,10 +32,14 @@ b_t = (R_2+R_g)/L
 
 print("t", q_t, "e", q_e, b_t )#"res", w_res )
 #abs. 1
+spek = np.arange(nu[0],nu[len(nu)-1],1)
+Amp = np.log(1/np.sqrt(
+(1-2.098*10.11e-12*((2*const.pi*spek)**2))**2
++(2*const.pi*spek*509.5*2.098e-9)**2
+))
 
-plt.plot(nu ,U_c_log ,'rx' ,label='Messwerte' )
-plt.plot(nu ,U_c_log ,'b-' ,label='verbundene Werte' )
-
+plt.plot(nu, U_c_log, 'rx',label='Messwerte')
+plt.plot(spek, Amp,'g-', label='theorie')
 
 #plt.axhline(U_b)
 #plt.axvline
